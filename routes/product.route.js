1acf343d67e210b06386c8bbfee31c80aa6906a2
marialobillo <maria.lobillo.santos@gmail.com>
2018-09-27 20:23:33 +0200

    routes

---
+++ routes/product.route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Require the controllers
+const product_controller =
+require('../controllers/product.controller');
+
+
+module.exports = router;
